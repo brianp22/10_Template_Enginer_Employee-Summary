bffaaa505e5113d49e1e3c84d53d898d5c58cfb3
Brian Prickril <bprickril22@gmail.com>
2020-02-04 19:47:20 -0500

    added write file

--- app.js
+++ app.js
@@ -15,25 +15,7 @@ const managerQ = [
         name: "officeNo"
     }
 ];
-
-const internQ = [
-    {
-        type: "input",
-        message: "What school is your Intern attending?",
-        name: "school"
-    }
-];
-
-const engineerQ = [
-    {
-        type: "input",
-        message: "What is your Engineer's GitHub?",
-        name: "github"
-    }
-];
-
-
-const EmployeeQuestions = [ 
+const EmployeeQuestions = [
     {
         type: "input",
         message: "What is the employee's name?",
@@ -65,90 +47,121 @@ const EmployeeQuestions = [
 // }
 
 
-async function start(){
+async function start() {
     console.log("Let's make your Dream Team!");
+    let teamHTML = "";
 
     await inquirer.prompt(
         {
-            type: "number",
-            message: "How many people are in your team?",
-            name: "noOfTeamMem"
-        }
-    )
-    .then((data) => {
-        teamSize = data.noOfTeamMem + 1;
-    });
+@@ -127, 28 + 96, 117 @@async function start() {
+        await inquirer.prompt(managerQ)
+            .then((data) => {
+                const manager = new Manager(name, id, email, data.officeNo);
+                team.push(manager);
 
-    // If Team Size is 0
-    if (teamSize === 0){
-        console.log("I guess there is no one on your team...");
-        return;
-    }
+                teamMember = fs.readFileSync("templates/manager.html");
 
-    for(i = 1; i < teamSize; i++){
+                teamHTML = teamHTML + "\n" + eval('`' + teamMember + '`');
 
-        let name;
-        let id;
-        let title;
-        let email;
-
-        await inquirer.prompt([ 
-            {
-                type: "input",
-                message: `What is employee (${i})'s name?`,
-                name: "name"
-            },
-            {
-                type: "input",
-                message: `What is the employee (${i})'s id?`,
-                name: "id"
-            },
-            {
-                type: "input",
-                message: `What is the employee (${i})'s Email?`,
-                name: "email"
-            },
-            {
-                type: "list",
-                message: `what the employee (${i})'s title?`,
-                name: "title",
-                choices: ["Engineer", "Intern", "Manager"]
-            }
-        ])
-        .then((data) => {
-            name = data.name;
-            id = data.id;
-            title = data.title;
-            email = data.email;
-        });
-
-        switch (title){
-            case "Manager":
-                await inquirer.prompt(managerQ)
-                .then((data) => {
-                    const manager = new Manager(name, id, email, data.officeNo);
-                    team.push(manager);
-                });
-                break;
+                // team.push(manager);
+            });
+        break;
             case "Intern":
-                await inquirer.prompt(internQ)
-                .then((data) => {
-                    const intern = new Intern(name, id, email, data.school);
-                    team.push(intern);
-                });
-                break;
+        await inquirer.prompt(internQ)
+            .then((data) => {
+                const intern = new Intern(name, id, email, data.school);
+                team.push(intern);
+
+                teamMember = fs.readFileSync("templates/intern.html");
+
+                teamHTML = teamHTML + "\n" + eval('`' + teamMember + '`');
+            });
+        break;
             case "Engineer":
-                await inquirer.prompt(engineerQ)
-                .then((data) => {
-                    const engineer = new Engineer(name, id, email, data.github);
-                    team.push(engineer);
-                });
-                break;
+        await inquirer.prompt(engineerQ)
+            .then((data) => {
+                const engineer = new Engineer(name, id, email, data.github);
+                team.push(engineer);
+
+                teamMember = fs.readFileSync("templates/engineer.html");
+
+                teamHTML = teamHTML + "\n" + eval('`' + teamMember + '`');
+
+            });
+        break;
+    }
+}
+
+console.log(team[0].name);
+console.log(team[0].getRole());
+//console.log(teamMember);
+
+
+
+teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+
+// team.push(manager);
+});
+break;
+case "Intern":
+@@ -128,6 +127,21 @@ async function start(){
+}
+}
+
+const mainHTML = fs.readFileSync("templates/main.html");
+
+teamHTML = eval('`'+ mainHTML +'`');
+
+fs.writeFile("output/DreamTeam.html", teamHTML, function(err) {
+
+if (err) {
+return console.log(err);
+}
+
+console.log("Success!");
+
+});
+
+
+//console.log(teamMember);
+
+
+
+console.log(teamHTML);
+    //generateTeamHTML(team);
+}
+
+async function generateTeamHTML(teamArray) {
+    let teamHTML;
+
+    for (i = 0; i < teamArray.length; i++) {
+
+        if (teamArray[i].getRole() == "Manager") {
+            fs.readFile("templates/engineer.html", "utf8", function (err, htmlFile) {
+                if (err) { return console.log(err); }
+                console.log(teamArray[i].name);
+                console.log(teamArray[i].getRole);
+                console.log(teamArray[i].id);
+                console.log(teamArray[i].email);
+                console.log(teamArray[i].officeNumber);
+                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
+            });
+        }
+        else if (teamArray[i].getRole() == "Intern") {
+            fs.readFile("templates/intern.html", "utf8", (err, htmlFile) => {
+                if (err) { return console.log(err); }
+                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
+            });
+        }
+        else if (teamArray[i].getRole() == "Engineer") {
+            fs.readFile("templates/engineer.html", "utf8", (err, htmlFile) => {
+                if (err) { return console.log(err); }
+                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
+            });
         }
     }
 
-    console.log(team[0].name);
-    console.log(team[0].getRole());
+    console.log(teamHTML);
 }
 
 start(); 
