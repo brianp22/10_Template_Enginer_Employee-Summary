d08ae869256edc464ced07577d4ac82f60a891e3
Brian Prickril <bprickril22@gmail.com>
2020-02-05 21:26:52 -0500

    debug

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 
-async function start(){
+async function init(){
     console.log("Get read to build you team!");
 
-
-    let teamHTML = "";
-    let teamSize;
+    let teamTemplates = "";
+    let teamNum;
 
     await inquirer.prompt(
         {
             type: "number",
             message: "How many people are in your team?",
-            name: "noOfTeamMem"
+            name: "teamNum"
         }
     )
     .then((data) => {
-        teamSize = data.noOfTeamMem + 1;
+        teamNum = data.teamNum + 1;
     });
 
-    if (teamSize === 0){
+    if (teamNum === 0){
         console.log("No team? Let's wrap this up then...");
         return;
     }
 
-    for(i = 1; i < teamSize; i++){
+    for(i = 1; i < teamNum; i++){
 
         let name;
         let id;
@@ -72,13 +70,13 @@ async function start(){
                     {
                         type: "input",
                         message: "What is your Manager's Office Number?",
-                        name: "officeNo"
+                        name: "officeNumber"
                     }
                 ])
                 .then((data) => {
-                    const manager = new Manager(name, id, email, data.officeNo);
+                    const manager = new Manager(name, id, email, data.officeNumber);
                     teamMember = fs.readFileSync("templates/manager.html");
-                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                    teamTemplates = teamTemplates + "\n" + eval('`'+ teamMember +'`');
                 });
                 break;
 
@@ -93,7 +91,7 @@ async function start(){
                 .then((data) => {
                     const intern = new Intern(name, id, email, data.school);
                     teamMember = fs.readFileSync("templates/intern.html");
-                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                    teamTemplates = teamTemplates + "\n" + eval('`'+ teamMember +'`');
                 });
                 break;
 
@@ -108,7 +106,7 @@ async function start(){
                 .then((data) => {
                     const engineer = new Engineer(name, id, email, data.github);
                     teamMember = fs.readFileSync("templates/engineer.html");
-                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                    teamTemplates = teamTemplates + "\n" + eval('`'+ teamMember +'`');
                 });
                 break;
 
@@ -118,18 +116,15 @@ async function start(){
 
     const mainHTML = fs.readFileSync("templates/main.html");
     
-    teamHTML = eval('`'+ mainHTML +'`');
+    teamTemplates = eval('`'+ mainHTML +'`');
 
-    fs.writeFile("output/team.html", teamHTML, function(err) {
+    fs.writeFile("output/employee_summary.html", teamTemplates, function(err) {
 
         if (err) {
           return console.log(err);
         }
-      
-        console.log("Success!");
-      
+        console.log("Congratulations! You've created your team!");
       });
 }
 
-
-start();
+init();
